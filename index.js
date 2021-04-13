fe42a920629b36fe7bcc00e99bd9cb004b93b5bf
Yoannes <yoannes@gmail.com>
2021-04-14 08:05:44 +0900

    monte de atualizacoes

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 class Carrossel {
   constructor(obj) {
     this._els = [];
+    this._dots = [];
     this._current = 0;
     this._imgContainer = document.createElement("div");
     this._moveLeft = document.createElement("div");
     this._moveRight = document.createElement("div");
+    this._dotsContainer = document.createElement("div");
+    this._svgIcons();
+
+    this._imgContainer.classList.add("__carrossel-img-container");
 
     if (obj.images) {
       this.images = obj.images;
@@ -54,11 +59,19 @@ class Carrossel {
       this._els.push(el);
     }
 
+    // Create the dot
+    const dotEl = document.createElement("div");
+    dotEl.style.width = "10px";
+    dotEl.style.height = "10px";
+    dotEl.style.border = "1px solid black";
+    dotEl.style.background = "black";
+    this._dots.push(dotEl);
+
     return el;
   }
 
   createElements() {
-    this._moveLeft.innerHTML = "<";
+    this._moveLeft.appendChild(this._chevronLeft);
     this._moveLeft.classList.add("__carrossel-move-left");
     this._moveLeft.onclick = () => {
       this._current--;
@@ -70,7 +83,7 @@ class Carrossel {
       this.showImage();
     };
 
-    this._moveRight.innerHTML = ">";
+    this._moveRight.appendChild(this._chevronRight);
     this._moveRight.classList.add("__carrossel-move-right");
     this._moveRight.onclick = () => {
       this._current++;
@@ -81,6 +94,36 @@ class Carrossel {
 
       this.showImage();
     };
+  }
+
+  _svgIcons() {
+    // Cria chevron esquerda
+    this._chevronLeft = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    this._chevronLeft.setAttributeNS(null, "style", "width:24px;height:24px");
+    this._chevronLeft.setAttributeNS(null, "viewBox", "0 0 24 24");
+
+    const chevronLeftPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    chevronLeftPath.setAttributeNS(null, "fill", "currentColor");
+    chevronLeftPath.setAttributeNS(
+      null,
+      "d",
+      "M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z"
+    );
+    this._chevronLeft.appendChild(chevronLeftPath);
+
+    // Cria chevron direita
+    this._chevronRight = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    this._chevronRight.setAttributeNS(null, "style", "width:24px;height:24px");
+    this._chevronRight.setAttributeNS(null, "viewBox", "0 0 24 24");
+
+    const chevronRightPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    chevronRightPath.setAttributeNS(null, "fill", "currentColor");
+    chevronRightPath.setAttributeNS(
+      null,
+      "d",
+      "M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"
+    );
+    this._chevronRight.appendChild(chevronRightPath);
   }
 
   showImage() {
